c1a42c905d523f4b2b02f57d6cc00f9472f7b968
joohyun kyong <joohyun0115@gmail.com>
2014-02-27 09:44:32 +0900

    ci : add test feature using simulator log

--- scripts/ci/tests/test_installation.py
+++ scripts/ci/tests/test_installation.py
@@ -1,7 +1,21 @@
+import os
+import sys
+from os.path import join
 from nose.tools import ok_, eq_
 
+PROJRELROOT = '../'
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), PROJRELROOT)))
+sys.path.append(os.path.abspath('./'))
+
+import result_parser
+
+the_product = str(os.getenv('TARGET_PRODUCT'))
+the_parser = result_parser.ResultParser()
+the_parser.ParseLog(the_product)
+    
 def test_flash():
     eq_(True, True)
 
 def test_boot():
-    eq_(True, True)
+    ret = the_parser.GetLogResult('INSTALLATION', 'BOOT')
+    eq_(ret, True)
